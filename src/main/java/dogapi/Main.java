40e8527c9b800c9c9c925de03b192ac6ae835c62
wenxuan <wenxuan.ding@mail.utoronto.ca>
2025-10-14 12:11:34 -0400

    When breed does not exist, return -1 instead of 0

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -33,9 +33,9 @@ public class Main {
             return subBreeds.size();
 
         } catch (BreedFetcher.BreedNotFoundException e) {
-            // breed does not exist, print hint and return 0.
+            // breed does not exist, print hint and return -1.
             System.out.println("Breed not found: " + breed);
-            return 0;
+            return -1;
         }
 
     }
